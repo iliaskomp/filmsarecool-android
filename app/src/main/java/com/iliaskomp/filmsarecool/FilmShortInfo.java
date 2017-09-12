f733fbe6e4c724e4216166ba73c00e0c1bf8d6cf
Ilias Kompogiannis <ilk@tractive.com>
2017-09-12 21:16:21 +0200

    Add MainActivity
    - Make API call to TMDb
    - Use Gson to deserialize

---
+++ app/src/main/java/com/iliaskomp/filmsarecool/FilmShortInfo.java
@@ -0,0 +1,22 @@
+package com.iliaskomp.filmsarecool;
+
+/**
+ * Created by IliasKomp on 12/09/17.
+ */
+
+public class FilmShortInfo {
+    private int voteCount;
+    private int id;
+    private boolean video;
+    private double voteAverage;
+    private String title;
+    private double popularity;
+    private String posterPath;
+    private String originalLanguage;
+    private String originalTitle;
+    private int[] genreIds;
+    private String backdropPath;
+    private String adult;
+    private String overview;
+    private String releaseDate;
+}
